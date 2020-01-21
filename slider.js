d744c2b21de2363d6de14da026a6b652ce7d0104
kseniakopli <55592072+kseniakopli@users.noreply.github.com>
2020-01-21 20:40:02 +0300

    add switching between dots and headers

--- slider.js
+++ slider.js
@@ -1,11 +1,10 @@
 //Получение данных
 const prevSlide = document.getElementById("prev-slide");
 const nextSlide = document.getElementById("next-slide");
-const sliderImages = document.getElementById("slider-images");
+const sliderImage = document.getElementById("slider-image");
 const sliderHeaders = document.getElementById("slider-headers");
 
 const dots = document.querySelectorAll(".dot");
-const imgs = sliderImages.children;
 const headers = sliderHeaders.children;
 
 const city = document.querySelectorAll(".city");
@@ -13,72 +12,83 @@ const apartmentArea = document.querySelectorAll(".apartment-area");
 const repairTime = document.querySelectorAll(".repair-time");
 const repairCost = document.querySelectorAll(".repair-cost");
 
-//Установки при загрузке
+const sliderInfo = [
+  {
+    src: "img/image1.jpg",
+    city: "Rostov-on-Don<br/>LCD admiral",
+    apartmentArea: "81 m<sup>2</sup>",
+    repairTime: "3.5 months",
+    repairCost: "Upon request"
+  },
+  {
+    src: "img/image2.jpg",
+    city: "Sochi<br/>Thieves",
+    apartmentArea: "105 m<sup>2</sup>",
+    repairTime: "4 months",
+    repairCost: "Upon request"
+  },
+  {
+    src: "img/image3.jpg",
+    city: "Rostov-on-Don<br />Patriotic",
+    apartmentArea: "93 m<sup>2</sup>",
+    repairTime: "3 months",
+    repairCost: "Upon request"
+  }
+];
 
-let currentIndexSrc = 0;
-
-imgs[currentIndexSrc].classList.remove("hide");
-city[currentIndexSrc].classList.remove("hide");
-apartmentArea[currentIndexSrc].classList.remove("hide");
-repairTime[currentIndexSrc].classList.remove("hide");
-repairCost[currentIndexSrc].classList.remove("hide");
-headers[currentIndexSrc].classList.add("active");
-dots[currentIndexSrc].classList.add("active");
+let currentIndex = 0;
 
 //события
-prevSlide.addEventListener("click", onPrevSlideClick);
-nextSlide.addEventListener("click", onNextSlideClick);
-
-//dots.addEventListener("click", changeSlide);
-//headers.addEventListener("click", changeSlide);
+prevSlide.addEventListener("click", function(event) {
+  event.preventDefault();
+  onPrevSlideClick();
+});
+nextSlide.addEventListener("click", function(event) {
+  event.preventDefault();
+  onNextSlideClick();
+});
+for (let i = 0; i < sliderInfo.length; i++) {
+  dots[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    applySettings(i);
+  });
+  headers[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    applySettings(i);
+  });
+}
 
 //функции
-function onPrevSlideClick() {
-  event.preventDefault();
+function applySettings(slide) {
+  sliderImage.src = sliderInfo[slide].src;
+  city.textContent = sliderInfo[slide].city;
+  apartmentArea.textContent = sliderInfo[slide].apartmentArea;
+  repairTime.textContent = sliderInfo[slide].repairTime;
+  repairCost.textContent = sliderInfo[slide].repairCost;
 
-  eraseSlides()
-
-  if (currentIndexSrc > 0) {
-    currentIndexSrc--;
-  } else {
-    currentIndexSrc = imgs.length - 1;
+  for (let i = 0; i < sliderInfo.length; i++) {
+    headers[i].classList.remove("active");
+    dots[i].classList.remove("active");
   }
-  setSlides()
+  headers[slide].classList.add("active");
+  dots[slide].classList.add("active");
+  currentIndex = slide;
+}
+
+function onPrevSlideClick() {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = sliderInfo.length - 1;
+  }
+  applySettings(currentIndex);
 }
 
 function onNextSlideClick() {
-  event.preventDefault();
-
-  eraseSlides()
-
-  if (currentIndexSrc < imgs.length - 1) {
-    currentIndexSrc++;
+  if (currentIndex < sliderInfo.length - 1) {
+    currentIndex++;
   } else {
-    currentIndexSrc = 0;
+    currentIndex = 0;
   }
-  setSlides()
-}
-
-/*function changeSlide() {
-  event.preventDefault();
-}*/
-
-function eraseSlides() {
-  imgs[currentIndexSrc].classList.add("hide");
-  headers[currentIndexSrc].classList.remove("active");
-  dots[currentIndexSrc].classList.remove("active");
-  city[currentIndexSrc].classList.add("hide");
-  apartmentArea[currentIndexSrc].classList.add("hide");
-  repairTime[currentIndexSrc].classList.add("hide");
-  repairCost[currentIndexSrc].classList.add("hide");
-}
-
-function setSlides() {
-  imgs[currentIndexSrc].classList.remove("hide");
-  headers[currentIndexSrc].classList.add("active");
-  dots[currentIndexSrc].classList.add("active");
-  city[currentIndexSrc].classList.remove("hide");
-  apartmentArea[currentIndexSrc].classList.remove("hide");
-  repairTime[currentIndexSrc].classList.remove("hide");
-  repairCost[currentIndexSrc].classList.remove("hide");
+  applySettings(currentIndex);
 }
